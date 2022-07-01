47d623b3857d8d2d83216762275078bb4f7ba339
elmorshdi53 <elmorshdi53@std.mans.edu.eg>
2022-07-01 14:28:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calculator/CalculatorAction.kt
@@ -0,0 +1,11 @@
+package com.example.calculator
+
+
+sealed class CalculatorAction {
+    data class Number(val number: Int): CalculatorAction()
+    object Clear: CalculatorAction()
+    object Delete: CalculatorAction()
+    data class Operation(val operation: CalculatorOperation): CalculatorAction()
+    object Calculate: CalculatorAction()
+    object Decimal: CalculatorAction()
+}
---
+++ app/src/main/java/com/example/calculator/CalculatorOperation.kt
@@ -0,0 +1,9 @@
+package com.example.calculator
+
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add: CalculatorOperation("+")
+    object Subtract: CalculatorOperation("-")
+    object Multiply: CalculatorOperation("x")
+    object Divide: CalculatorOperation("/")
+}
